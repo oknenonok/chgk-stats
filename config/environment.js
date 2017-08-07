ed2257a90b92c4d5c73ee97b2eaccbe541447ff5
oknenonok <13841@7733.ru>
2017-08-08 00:16:03 +1000

    все-таки hash

--- config/environment.js
+++ config/environment.js
@@ -6,7 +6,7 @@ module.exports = function(environment) {
     modulePrefix: 'chgk-stats',
     environment,
     rootURL: '',
-    locationType: 'auto',
+    locationType: 'hash',
     defaultCity: 'Владивосток',
     EmberENV: {
       FEATURES: {
